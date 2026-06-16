e35e853192f48ae28a6fb46e5ba187bd4b956b63
Adam Munawar Rahman <msrahmanadam@gmail.com>
2026-06-16 13:38:23 -0400

    Release 0.8.0

--- src/anodize_mcp/__init__.py
+++ src/anodize_mcp/__init__.py
@@ -81,7 +81,7 @@ from .server.middleware import (
 from .tools.tool import ToolResult
 from .utilities.types import Audio, File, Image
 
-__version__ = "0.7.0"
+__version__ = "0.8.0"
 
 __all__ = [
     "AnodizeMCP",
